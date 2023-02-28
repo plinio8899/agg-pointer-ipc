f4213dd654ae85f1c2542bc4d7a938d50aacf173
NeonBlack89 <45135662+plinio8899@users.noreply.github.com>
2023-02-28 11:59:11 -0500

    Se inicia el primer commit

---
+++ script.js
@@ -0,0 +1,53 @@
+function agregarHombres(){
+    let points = document.getElementById("points").value;
+    let description = document.getElementById("description").value; 
+    
+    if(!points || !description){
+        alert("Uno de los campos se encuentra vacio")
+    }else{
+        let data = {
+            number: points,
+            description: description
+        }
+        console.log(data)
+        fetch("https://jovenespdc.herokuapp.com/h-points/", {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers:{
+                'Content-Type': 'application/json; charset=UTF-8',
+                'Accept': '*/*'
+            },
+
+        }).then(res => res.json())
+        .catch(error => console.error('Error:', error))
+        .then(response => console.log('Success:', response));
+    }
+
+}
+
+function agregarMujeres(){
+    let points = document.getElementById("points").value;
+    let description = document.getElementById("description").value; 
+    
+    if(!points || !description){
+        alert("Uno de los campos se encuentra vacio")
+    }else{
+        let data = {
+            number: points,
+            description: description
+        }
+        console.log(data)
+        fetch("https://jovenespdc.herokuapp.com/m-points/", {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers:{
+                'Content-Type': 'application/json; charset=UTF-8',
+                'Accept': '*/*'
+            },
+
+        }).then(res => res.json())
+        .catch(error => console.error('Error:', error))
+        .then(response => console.log('Success:', response));
+    }
+
+}
